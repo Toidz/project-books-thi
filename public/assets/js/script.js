a3fcdf20ff65f1f36c3e7f06e8f908838d16c948
Nguyen Dat <tymmin146@gmail.com>
2025-10-23 16:30:55 +0700

    address

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -967,7 +967,7 @@ if(innerFillAddress1){
       },
       {
         rule: 'customRegexp',
-        value: /^(0|\+84)(3|5|7|8|9)[0-9]{8}$/,
+        value: /^0(3|5|7|8|9)[0-9]{9}$/,
         errorMessage: 'Số điện thoại không hợp lệ! Vui lòng nhập đúng định dạng.',
       },
     ])
@@ -1235,7 +1235,7 @@ if(innerFillAddress){
       },
       {
         rule: 'customRegexp',
-        value: /^(0|\+84)(3|5|7|8|9)[0-9]{8}$/,
+         value: /^0(3|5|7|8|9)[0-9]{9}$/,
         errorMessage: 'Số điện thoại không hợp lệ! Vui lòng nhập đúng định dạng.',
       },
     ])
@@ -1324,7 +1324,7 @@ if(innerAddNewAddress){
       },
       {
         rule: 'customRegexp',
-        value: /^(0|\+84)(3|5|7|8|9)[0-9]{8}$/,
+        value: /^0(3|5|7|8|9)[0-9]{9}$/,
         errorMessage: 'Số điện thoại không hợp lệ! Vui lòng nhập đúng định dạng.',
       },
     ])
